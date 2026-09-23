486552657599e3e8637614d041ae6fec5559c051
mbarreirovidal@danielcastelao.org <mbarreirovidal@danielcastelao.org>
2026-09-23 09:38:18 +0200

    tipos de datos y comentarios

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -33,8 +33,31 @@ fun main() {
     cartTotal = 20
     println("Total: $cartTotal")
 
+    //incremento de valor
     var count = 10
     println("You have $count unread messages.")
     count = count + 1
-    println("You have $count unread messages.")
+
+    //double
+    val viaje1: Double = 3.20
+    val viaje2: Double = 4.10
+    val viaje3: Double = 1.72
+    val duracionTotalViaje: Double = viaje1 + viaje2 + viaje3
+    println("$duracionTotalViaje kilómetros para llegar a tu destino.")
+
+    val proximaReunion = "Proxima Reunion: "
+    val fecha = "1 de enero"
+    val recordatorio = proximaReunion + fecha + " de 2027."
+    println(recordatorio)
+
+    println("Hola \"que tal\"")
+
+    //boolean
+    val activarNotisT: Boolean = true
+    println(activarNotisT)
+
+    val activarNotisF: Boolean = false
+    println(activarNotisF)
+
+
 }
