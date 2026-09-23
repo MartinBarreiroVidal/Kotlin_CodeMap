c43f559fa33b06a61167833822d06627a218337f
mbarreirovidal@danielcastelao.org <mbarreirovidal@danielcastelao.org>
2026-09-23 09:27:41 +0200

    variables y count

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,40 @@
-fun main(){
-    println("Hello World!")
+fun main() {
+    println("Hello, Android!")
+    println("Hello, Martín Barreiro!")
 
+    println("1")
+    println("2")
+    println("3")
+
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+
+
+    val cont: Int = 10
+    println("You have $cont unread messages.")
+
+    val numFotos = 100
+    val fotosBorradas = 10
+    println("$numFotos fotos.")
+    println("$fotosBorradas fotos borradas.")
+    println("${numFotos - fotosBorradas} fotos restantes.")
+
+
+    var cartTotal = 0
+    println("Total: $cartTotal")
+
+    cartTotal = 20
+    println("Total: $cartTotal")
+
+    var count = 10
+    println("You have $count unread messages.")
+    count = count + 1
+    println("You have $count unread messages.")
 }
